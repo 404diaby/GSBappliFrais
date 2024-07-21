84c14e2d505d4fdcab9c8e70d90e19f9ee5c3f38
Diaby Mamadou <85366150+404diaby@users.noreply.github.com>
2024-07-21 23:25:20 +0200

    Delete Nouveau dossier directory

--- Nouveau dossier/vues/v_pied.php
+++
@@ -1,7 +0,0 @@
-
-<!-- Division pour le pied de page -->
-<div>
-   <hr /><p style="text-align:center;"><img src="images/castel.png" style="vertical-align: middle;"><?php echo "Lyc&eacute;e Le Castel &agrave; Dijon - BTS SIO - <img src='images/copyleft.png' style='text-align: center; vertical-align: middle;'> janvier 2021 Blain Pascal";?> <a href="page.html"> Mention légal</a></p> 
-</div>
-  </body>
-</html>
